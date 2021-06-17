b8c32d5f1a72d178dbd8afb0975195880d6421bd
Humaira Zafar <root@Humairas-Air.attlocal.net>
2021-06-17 00:18:33 -0500

    Comit6: ALl Tweets are visible at the index route

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+   
+    def index
+     tweets = Tweet.all
+     render json:{status:200, tweets: tweets}
+
+    end
+
+    def show
+    
+    end
+
+
+end
