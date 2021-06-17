66adf97e8edbaa9efbc999d964e64fbc0af41d4b
Humaira Zafar <root@Humairas-Air.attlocal.net>
2021-06-17 00:04:55 -0500

    Commit4:Created model and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+Tweet.create(
+  title: Faker::Hipster.sentences(number:3),
+  content: Faker::Lorem.paragraph,
+  author: Faker:: Team.name
+)
+end
+
